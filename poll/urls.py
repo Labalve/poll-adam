cfd6af16dbb1d0473e5f83eb778c4e7b63cdc259
Krzysztof Kulak <krzysztof.kulak@sollers.eu>
2019-02-03 21:36:39 +0100

    Added person tracking and basic style

--- poll/urls.py
+++ poll/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/<str:person_id>', views.detail, name='detail'),
     path('results/', views.results, name='results'),
     path('<int:question_id>/answer/<person_id>', views.answer, name='answer'),
     path('finish/', views.finish, name='finish')
--- poll/views.py
+++ poll/views.py
@@ -1,21 +1,33 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+import random
+import string
 from django.template import loader
 from .models import QuestionSet, Question, Answer, Choice
 
 
 def index(request):
+    person_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(1, 21)])
     context = {
         'welcome_message': "Henlo to my website",
-        'first_question_id': QuestionSet.objects.get(name="aktywny").question_set.first().id
+        'first_question_id': QuestionSet.objects.get(name="aktywny").question_set.first().id,
+        'person_id': person_id
     }
     return render(request, 'index.html', context)
 
 
-def detail(request, question_id):
+def detail(request, question_id, person_id=False):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'detail.html', {'question': question})
+    if not person_id:
+        random = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(1, 21)])
+        question = get_object_or_404(Question, pk=question_id)
+        person_id = random
+    context = {
+        'question': question,
+        'person_id': person_id
+    }
+    return render(request, 'detail.html', context)
 
 
 def results(request, question_id):
@@ -33,7 +45,7 @@ def answer(request, question_id, person_id):
         Answer.create_closed(person_id, choice, question)
     if next_question.id == question_id:
         return HttpResponseRedirect(reverse('finish', args=()))
-    return HttpResponseRedirect(reverse('detail', args=(next_question.id,)))
+    return HttpResponseRedirect(reverse('detail', args=(next_question.id, person_id,)))
 
 
 def finish(request):
