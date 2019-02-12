c5ff3798f16e0e6c727ec04a5cca9989314c27f1
Krzysztof Kulak <krzysztof.kulak@sollers.eu>
2019-02-12 18:06:02 +0100

    added steps to realese

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,2 +1,3 @@
+python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
